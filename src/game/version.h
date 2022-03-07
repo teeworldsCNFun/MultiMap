2b84336e4eb92f89d4f3f5d5e3523766048428cd
ST-Chara <chara_kill_sans@163.com>
2022-03-07 18:34:20 +0800

    made it 0/6/5

--- src/game/version.h
+++ src/game/version.h
@@ -3,8 +3,8 @@
 #ifndef GAME_VERSION_H
 #define GAME_VERSION_H
 #ifndef NON_HASED_VERSION
-#define GAME_VERSION "0.6.4"
+#define GAME_VERSION "0.6.5"
 #define GAME_NETVERSION "0.6 626fce9a778df4d4" // the std game version. xD
-static const char GAME_RELEASE_VERSION[8] = "0.6.4";
+static const char GAME_RELEASE_VERSION[8] = "0.6.5";
 #endif
 #endif
